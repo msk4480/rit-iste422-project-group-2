750031a724bf2eafd5672001ffa58641ee749afe
Replit user <>
2023-10-19 19:27:57 +0000

    runedgeconvert changed

--- src/main/java/RunEdgeConvert.java
+++ src/main/java/RunEdgeConvert.java
@@ -1,5 +1,11 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class RunEdgeConvert {
-   public static void main(String[] args) {
-      EdgeConvertGUI edge = new EdgeConvertGUI();
-   }
+	 private static Logger logger = LogManager.getLogger(RunEdgeConvert.class.getName());
+
+	 public static void main(String[] args) {
+			logger.info("Starting application {}", RunEdgeConvert.class);
+			EdgeConvertGUI edge = new EdgeConvertGUI();
+	 }
 }
