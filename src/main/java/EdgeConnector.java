1b5f1cb4b6832731df23d47ab6b17c89100fdf2d
ajg3062 <70306205+ajg3062@users.noreply.github.com>
2023-10-22 20:47:27 +0000

    Added logger to EdgeConnector and EdgeConvertGUI
    Added several logger statements to these two java files

--- src/main/java/EdgeConnector.java
+++ src/main/java/EdgeConnector.java
@@ -1,11 +1,17 @@
 import java.util.StringTokenizer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class EdgeConnector {
+
+   private static Logger logger = LogManager.getLogger(EdgeConnector.class.getName());
+  
    private int numConnector, endPoint1, endPoint2;
    private String endStyle1, endStyle2;
    private boolean isEP1Field, isEP2Field, isEP1Table, isEP2Table;
       
    public EdgeConnector(String inputString) {
+      logger.info("EdgeConnector Constructor called w/" + inputString);
       StringTokenizer st = new StringTokenizer(inputString, EdgeConvertFileParser.DELIM);
       numConnector = Integer.parseInt(st.nextToken());
       endPoint1 = Integer.parseInt(st.nextToken());
@@ -54,18 +60,22 @@ public class EdgeConnector {
    }
 
    public void setIsEP1Field(boolean value) {
+      logger.info("Set EP1Field to " + value);
       isEP1Field = value;
    }
    
    public void setIsEP2Field(boolean value) {
+      logger.info("Set EP2Field to " + value);
       isEP2Field = value;
    }
 
    public void setIsEP1Table(boolean value) {
+      logger.info("Set EP1Table to " + value);
       isEP1Table = value;
    }
 
    public void setIsEP2Table(boolean value) {
+      logger.info("Set EP2Table to " + value);
       isEP2Table = value;
    }
 }
